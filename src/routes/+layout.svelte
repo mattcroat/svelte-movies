c49c447cf608452e8854ab3f3b2656e0253e97eb
mattcroat <mattcroat.dev@gmail.com>
2022-09-17 17:54:16 +0200

    styles: Header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,4 +6,21 @@
 	<title>Svelte Movies</title>
 </svelte:head>
 
-<slot />
+<header class="flex py-5 px-16 items-center gap-16 border-b-2 border-gray-800">
+	<h1 class="font-bold flex gap-1 items-center text-xl">
+		<img class="w-10 h-10" src="/logo.svg" alt="Svelte logo" />
+		<div>
+			svelte <span class="text-orange-500">movies</span>
+		</div>
+	</h1>
+
+	<nav class="capitalize flex gap-4">
+		<a href="/movies">movies</a>
+		<a href="/shows">TV shows</a>
+		<a href="/people">people</a>
+	</nav>
+</header>
+
+<main class="p-16">
+	<slot />
+</main>
